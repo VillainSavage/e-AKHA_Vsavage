704cc37f0b45bd298d64a2ee4edf30f1298a78c1
MissVillain <adilahfitrah@gmail.com>
2026-07-09 04:24:26 +0800

    Display KZ Koperal offdays from June hakiki schedule

--- jun-2026/loader.js
+++ jun-2026/loader.js
@@ -7,5 +7,12 @@
     "/function parseRow\\(layer,row,rowNo\\)\\{[\\s\\S]*?\\}function renderStaging/",
     "/function parseRow\\(layer,row,rowNo\\)\\{[\\s\\S]*?\\}\\s*function renderStaging/"
   );
+  const offdayPatch=[
+    "code=code.replace(/function renderAudit\\(\\)/,'function kzkDisplay(member,day,value){return shiftFor(member,day)===\\'OFF\\'?\\'OFFDAY\\':normalizeKz(value)}\\nfunction renderAudit()');",
+    "code=code.replace(/<td>\\$\\{normalizeKz\\(c\\.kzk\\)\\}<\\/td>/g,'<td>${kzkDisplay(m,d,c.kzk)}</td>');",
+    "code=code.replace(/normalizeKz\\(c\\.kzk\\),tcAudit\\(m,d,c\\)\\.text/g,'kzkDisplay(m,d,c.kzk),tcAudit(m,d,c).text');",
+    "code=code.replace(/if\\(sh==='OFF'\\)return'OFF';/g,\"if(sh==='OFF')return'OFFDAY';\");"
+  ].join("\n");
+  patch=patch.replace("new Function(code)();\n})();",offdayPatch+"\nnew Function(code)();\n})();");
   new Function(patch)();
 })();
