578589dd1a742d8341184c00cb147822040fef1e
MissVillain <adilahfitrah@gmail.com>
2026-07-09 01:26:38 +0800

    Fix June loader parser staging pattern

--- jun-2026/loader.js
+++ jun-2026/loader.js
@@ -1,1 +1,11 @@
-(()=>{const s=(window.__JUN_LOADER_B64_PARTS__||[]).join('');window.__JUN_LOADER_B64_PARTS__=[];const b=Uint8Array.from(atob(s),c=>c.charCodeAt(0));new Function(new TextDecoder().decode(b))();})();
+(()=>{
+  const s=(window.__JUN_LOADER_B64_PARTS__||[]).join('');
+  window.__JUN_LOADER_B64_PARTS__=[];
+  const b=Uint8Array.from(atob(s),c=>c.charCodeAt(0));
+  let patch=new TextDecoder().decode(b);
+  patch=patch.replace(
+    "/function parseRow\\(layer,row,rowNo\\)\\{[\\s\\S]*?\\}function renderStaging/",
+    "/function parseRow\\(layer,row,rowNo\\)\\{[\\s\\S]*?\\}\\s*function renderStaging/"
+  );
+  new Function(patch)();
+})();
